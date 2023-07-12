c04f176c43ffa15859adad792e3f5894afdda24a
Mieu Teacher <mieuteacher@gmail.com>
2023-07-12 19:46:08 +0700

    init

---
+++ src/firebase.config.js
@@ -0,0 +1,74 @@
+const { initializeApp } = require("firebase/app");
+const { getStorage, ref, uploadBytes, getDownloadURL, listAll } = require("firebase/storage");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2-lFmFa-JCNt3psT2PZ5EGomGDO8U8r4",
+  authDomain: "meo-farmer.firebaseapp.com",
+  projectId: "meo-farmer",
+  storageBucket: "meo-farmer.appspot.com",
+  messagingSenderId: "358148035486",
+  appId: "1:358148035486:web:b7a64850f0be4766b33c8f",
+  measurementId: "G-BDSCGF59W2"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
+
+/* 
+  1st param: your file, 2nd param: folder you need 
+  Returns: 
+    - If failed => false
+    - If success => URL of the uploaded file
+*/
+async function uploadFileToStorage(fileUploads, folderName) { 
+
+  // If the file is null, do nothing
+  if (!fileUploads) { 
+    return false;
+  }
+
+  // Convert the file to a format suitable for Firebase upload
+  const fileRef = ref(storage, `${folderName}/` + fileUploads.name);
+
+  // Upload the file to Firebase storage
+  let url = await uploadBytes(fileRef, fileUploads).then(async (res) => {
+    // When the upload is successful, retrieve the URL
+    return await getDownloadURL(res.ref)
+      .then((url) => url)
+      .catch((er) => false);
+  });
+
+  return url;
+}
+
+/* 
+  Only parameter: folder name
+  Returns: 
+    - If failed => false
+    - If success => array of URL links
+*/
+async function getFileInFolder(folderName) {
+  const listRef = ref(storage, folderName);
+
+  return await listAll(listRef).then(async (res) => {
+    let result = []; // Create an empty array
+
+    for (let i in res.items) { 
+      let url = await getDownloadURL(res.items[i])
+        .then((url) => url)
+        .catch((er) => false);
+      if (!url) {
+        return false;
+      }
+      result.push(url);
+    }
+
+    return result;
+  });
+}
+
+module.exports = {
+  uploadFileToStorage,
+  getFileInFolder
+};
---
+++ src/meomeo.js
@@ -0,0 +1,110 @@
+var http = require("http");
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var Multiparty = require('multiparty');
+
+http.createServer((request, response) => {
+    let urlObj = url.parse(request.url, true)
+
+    switch( "/" + urlObj.pathname.split('/')[1]) {
+        case '/':
+            let homePath = path.join(__dirname, "templates/home.html");
+        
+            fs.readFile(homePath, 'utf-8', (err, data) => {
+                if (err) {
+
+                    response.writeHead(400, { 'Content-Type': 'text/plain' })
+
+                    response.end("Trang Chủ")
+                }else {
+
+                    response.writeHead(200, { 'Content-Type': 'text/html' })
+                    response.end(data)
+                }
+            })
+
+            break
+        case '/formui':
+            let formPath = path.join(__dirname, "templates/form.html");
+        
+            fs.readFile(formPath, 'utf-8', (err, data) => {
+                if (err) {
+
+                    response.writeHead(400, { 'Content-Type': 'text/plain' })
+
+                    response.end("Load form failed")
+                }else {
+
+                    response.writeHead(200, { 'Content-Type': 'text/html' })
+                    response.end(data)
+                }
+            })
+
+            break
+        case '/formjs':
+            if (request.method == "GET") {
+                console.log("đã vào formjs", urlObj.query.username, urlObj.query.password)
+            }
+
+            if (request.method == "POST") {
+                const form = new Multiparty.Form();
+
+                form.parse(request, async (err, fields, files) => {
+                    if (err) {
+                    // Xử lý lỗi
+                    console.error(err);
+                    response.statusCode = 400;
+                    response.end('Lỗi xử lý dữ liệu');
+                    return;
+                    }
+                    
+                    let userInfor = {
+                        userName: fields.userName[0],
+                        passWord: fields.passWord[0],
+                    }
+
+                    
+
+                    let uploadedFile = files.userAvatar[0];
+
+                    let targetPath = path.join(__dirname, `images/${uploadedFile.originalFilename}`)
+
+                    fs.copyFile(uploadedFile.path, targetPath, (err) => {
+                        if (err) {
+                            console.error(err);
+                            response.statusCode = 500;
+                            response.end('Lỗi sao chép file');
+                            return;
+                        }
+                
+                        console.log('File đã được lưu:', targetPath);
+
+                        // Tiếp tục xử lý các thông tin khác hoặc trả về phản hồi thành công
+                        response.end("Da nhan thong tin")
+                    });
+                });
+            }
+            break
+        case '/images':
+            let imagePath = path.join(__dirname, `${urlObj.pathname}`);
+            fs.readFile(imagePath, (err, data) => {
+                if (err) {
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.end('File not found');
+                } else {
+                
+
+                response.writeHead(200, { 'Content-Type': `image/${urlObj.pathname.split('.')[urlObj.pathname.split('.').length - 1]}` });
+
+                response.end(data);
+                
+                }
+            });
+            
+            break;
+        default: 
+            response.writeHead(404, { 'Content-Type': 'text/plain' })
+            response.end("Trang khong ton tai")
+    }
+}).listen(2222)
